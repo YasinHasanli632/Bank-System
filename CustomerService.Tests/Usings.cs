e245287657cd339154a3607c13ccda5fc4c5450c
YasinHasanli632 <yasinhasanli632@gmail.com>
2026-01-09 15:47:22 +0400

    chore: clean repository and add proper .gitignore

--- CustomerService.Tests/Usings.cs
+++
@@ -1,8 +0,0 @@
-//global using Xunit;
-//global using Moq;
-//global using System.Threading.Tasks;
-//global using Microsoft.EntityFrameworkCore;
-//global using CustomerService.Application.Interfaces;
-//global using CustomerService.Application.Services;
-//global using CustomerService.Domain.Entities;
-//global using CustomerService.Infrastructure.Repositories;
